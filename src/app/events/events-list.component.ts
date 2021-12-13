11b2917d7b132c954a222e46bcd9d4fca4651a53
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-13 09:30:57 +0100

    external template

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,22 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  template: `
-    <div>
-      <h1>Upcoming Angular Events</h1>
-      <hr>
-      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-          <span>Location: {{event.location.address}}</span>
-          <span>&nbsp;</span>
-          <span>{{event.location.city}}, {{event.location.country}}</span>
-      </div>
-    </div>
-  `,
+  templateUrl: "./events-list.component.html",
 })
 export class EventsListComponent {
   event = {
