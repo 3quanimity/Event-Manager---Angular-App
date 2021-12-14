6bd22fb8a13241f3bceaa03669aa013c7b0b9948
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-14 08:17:43 +0100

    repeating data with *ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,19 +2,32 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
-  template: `
-  <div class="well hoverwell thumbnail">
-  <h2>{{event.name}}</h2>
-  <div>Date: {{event.date}}</div>
-  <div>Time: {{event.time}}</div>
-  <div>Price: \${{event.price}}</div>
-  <div>
-    <span>Location: {{event.location.address}}</span>
-    <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
-  </div>
-  </div>`
+  template: ` <div class="well hoverwell thumbnail">
+    <h2>{{ event.name }}</h2>
+    <div>Date: {{ event.date }}</div>
+    <div>Time: {{ event.time }}</div>
+    <div>Price: \${{ event.price }}</div>
+    <div>
+      <span>Location: {{ event.location.address }}</span>
+      <span class="pad-left"
+        >{{ event.location.city }}, {{ event.location.country }}</span
+      >
+    </div>
+  </div>`,
+  styles: [
+    `
+      .thumbnail {
+        min-height: 210px;
+      }
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `,
+  ],
 })
-
-export class EventThumbnailComponent{
+export class EventThumbnailComponent {
   @Input() event: any;
 }
