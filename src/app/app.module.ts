206c79e3e26f663d57aaeced5a8dfa24d9d82d5f
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-14 10:08:10 +0100

    wrapping a third party service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/events.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   imports: [
@@ -17,7 +18,7 @@ import { EventService } from './events/shared/events.service';
     EventThumbnailComponent,
     NavBarComponent
   ],
-  providers: [EventService],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/events.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'events-list',
@@ -9,7 +10,7 @@ import { EventService } from './shared/events.service';
       <hr />
       <div class="row">
         <div class="col-md-5" *ngFor="let event of events">
-          <event-thumbnail [event]="event"></event-thumbnail>
+          <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
         </div>
       </div>
     </div>
@@ -18,9 +19,13 @@ import { EventService } from './shared/events.service';
 export class EventsListComponent implements OnInit {
   events: any[] | undefined;
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private toastrService: ToastrService) {}
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName: any) {
+    this.toastrService.success(eventName);
   }
 }
