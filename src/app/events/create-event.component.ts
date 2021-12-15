2f344e3bf76182ba21856a5c12d8b9a040609494
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-15 08:17:15 +0100

    navigate from code (using event click)

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   template: `
@@ -9,12 +10,16 @@ import { Component } from '@angular/core';
       <br/>
       <br/>
       <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
+      <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
     </div>
   `
 })
 
 export class CreateEventComponent {
+  constructor(private router:Router) { }
 
+  cancel() {
+    this.router.navigate(['/events']);
+  }
 }
 
