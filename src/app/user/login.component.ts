d61fd0788a4296957a4c6bddb74aabdd05030af1
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-18 08:19:19 +0100

    validating (template-based) forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,16 @@ import { Router } from "@angular/router";
 
 @Component({
   templateUrl: "./login.component.html",
+  styles: [
+    `
+      em {float: right; color: #e05c65; padding-left: 10px;}
+    `
+  ]
 })
 export class LoginComponent {
   userName: string | undefined;
   password: string | undefined;
+  mouseoverLogin: boolean = false;
 
   constructor(public authService : AuthService, private router: Router) {}
 
