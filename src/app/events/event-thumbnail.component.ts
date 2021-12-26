09d980154da9d3b616be58c89287599b47a31d55
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-26 07:24:41 +0100

    using built-in pipes

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,15 +5,15 @@ import { IEvent } from './shared/index';
   selector: 'event-thumbnail',
   template: `
   <div [routerLink]="['/events', event?.id]" class="well hoverwell thumbnail">
-    <h2>{{ event?.name }}</h2>
-    <div>Date: {{ event?.date }}</div>
+    <h2>{{ event?.name  | uppercase}}</h2>
+    <div>Date: {{ event?.date | date:'shortDate' }}</div>
     <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
       Time: {{ event?.time }}
       <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
       <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
       <span *ngSwitchDefault>(Normal Start)</span>
     </div>
-    <div>Price: \${{ event?.price }}</div>
+    <div>Price: {{ event?.price | currency:'USD' }}</div>
     <div *ngIf="event?.location">
       <span>Location: {{ event?.location?.address }}</span>
       <span class="pad-left"
