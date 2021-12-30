95c93f0d04c3a971e1fb38948b8228e6a3bf6a1f
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-30 08:49:36 +0100

    sorting data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -16,6 +16,7 @@ export class EventDetailsComponent implements OnInit{
   event!: IEvent;
   addMode: boolean = false;
   filterBy: string = 'all';
+  sortBy: string = 'votes';
 
   constructor(private eventService: EventService, private route: ActivatedRoute) {}
 
--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,11 +8,13 @@ import { ISession } from "../shared/index";
 export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[] | undefined;
   @Input() filterBy: string = 'all';
+  @Input() sortBy: string = 'votes';
   visibleSessions: ISession[] | undefined;
 
   ngOnChanges(): void { // gonna be called when one of the inputs gets a new value
     if (this.sessions) {
-      this.filterSessions(this.filterBy)
+      this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions?.sort(sortByNameAsc) : this.visibleSessions?.sort(sortByVotesDesc);
     }
   }
 
@@ -23,4 +25,16 @@ export class SessionListComponent implements OnChanges {
       this.visibleSessions = this.sessions?.filter(session => session.level.toLocaleLowerCase() === filter);
     }
   }
+
+}
+
+// implemented outside of the class since they re stateless functions and dont need to be a method of the class
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) return 1;
+  else if (s1.name === s2.name) return 0;
+  else return -1;
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
