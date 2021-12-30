792c4af98ed3d2fd9ceb85abe37f20e9d3090775
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-30 08:21:21 +0100

    create a filtering display

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from "../shared/index";
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
-  @Input()
-  sessions: ISession[] | undefined;
+export class SessionListComponent implements OnChanges {
+  @Input() sessions: ISession[] | undefined;
+  @Input() filterBy: string = 'all';
+  visibleSessions: ISession[] | undefined;
+
+  ngOnChanges(): void { // gonna be called when one of the inputs gets a new value
+    if (this.sessions) {
+      this.filterSessions(this.filterBy)
+    }
+  }
+
+  filterSessions(filter:string) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions?.slice(0); // .slice(0) create a copy/clone of the array
+    } else {
+      this.visibleSessions = this.sessions?.filter(session => session.level.toLocaleLowerCase() === filter);
+    }
+  }
 }
