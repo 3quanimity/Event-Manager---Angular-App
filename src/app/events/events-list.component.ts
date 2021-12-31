97b0718d44db9b5bbc30eedbd64a3887eb9b25b8
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-31 08:05:47 +0100

    using Angular's InjectionToken

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/events.service';
-// import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
 import { IEvent } from '.';
 
