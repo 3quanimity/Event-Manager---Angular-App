06906bb05beaf0868e5581d44ce2f097ad1172aa
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-31 08:13:17 +0100

    using Angular's @Inject Decorator

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service'
 
 @Component({
   templateUrl: "./profile.component.html",
@@ -19,7 +20,10 @@ export class ProfileComponent implements OnInit {
   firstName!: FormControl
   lastName!: FormControl
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr) {}
 
   ngOnInit() {
     this.firstName = new FormControl(this.authService.currentUser?.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
@@ -33,7 +37,7 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues: any) {
     if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.router.navigate(['events'])
+      this.toastr.success('Profile Saved');
     }
   }
 
